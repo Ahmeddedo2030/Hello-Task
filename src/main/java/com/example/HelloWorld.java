804b521fa09a10d9b99cd46281e76f8ca45332ac
ahmeddedo <ahmed.dedo2030@gmail.com>
2025-04-28 15:12:13 +0200

    Deployment Hello Task

--- src/main/java/com/example/HelloWorld.java
+++ src/main/java/com/example/HelloWorld.java
@@ -5,13 +5,21 @@ import com.atlassian.bamboo.task.TaskException;
 import com.atlassian.bamboo.task.TaskResult;
 import com.atlassian.bamboo.task.TaskResultBuilder;
 import com.atlassian.bamboo.task.TaskType;
+import com.atlassian.bamboo.deployments.execution.DeploymentTaskContext;
+import com.atlassian.bamboo.deployments.execution.DeploymentTaskType;
 
-public class HelloWorld implements TaskType {
+public class HelloWorld implements TaskType, DeploymentTaskType {
 
 	@Override
-    public TaskResult execute(TaskContext taskContext) throws TaskException {
+    public TaskResult execute(TaskContext taskContext) throws TaskException{
         
         taskContext.getBuildLogger().addBuildLogEntry("Hello from MyTask!");
+        return TaskResultBuilder.newBuilder(taskContext).success().build();
+    }
+
+    @Override
+    public TaskResult execute(DeploymentTaskContext taskContext) throws TaskException {
+        taskContext.getBuildLogger().addBuildLogEntry("Hello from My Deployment Task!");
         return TaskResultBuilder.newBuilder(taskContext).success().build();
     }
 }
